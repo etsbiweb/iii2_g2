644b9e9003bf8e08b37966258b422b8a18e69bd0
Muslic.dev <sender.americanfood@gmail.com>
2025-05-22 10:53:46 +0200

    Uljepsan frontend za login page -RADIO ADIJAN HODZIC

--- login.php
+++ login.php
@@ -70,7 +70,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="login.css">
     <title>Login</title>
 </head>
 
@@ -87,35 +87,64 @@
             unset( $_SESSION['login_error'] );
         }
     ?>
-    <div class="container" id="container">
-        <div class="form-container sign-in">
-            <form action="login.php" method="post">
-                <a href="https://etsbi.edu.ba/" class="icon" target="_blank">
-                    <img src="imgs/dwa.png" alt="ETSBI" class="custom-icon">
+   <div class="container" id="container">
+        <div class="form-container sign-up">
+            <form>
+                <br></br>
                 </a>
-                <h1>Sign In</h1>
+                <h1>Create Account</h1>
                 <div class="social-icons">
-                    <a href="https://etsbi.edu.ba/" class="icon" target="_blank"><i class="fa-solid fa-graduation-cap"></i></a>
-                    <a href="https://www.facebook.com/etsbi.bihac/?locale=hr_HR" class="icon" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
-                    <a href="https://www.instagram.com/etsbi_/" class="icon" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="https://etsbi.edu.ba/" class="icon"><i class="fa-solid fa-graduation-cap"></i></a>
+                    <a href="https://www.facebook.com/etsbi.bihac/?locale=hr_HR" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="https://www.instagram.com/etsbi_/" class="icon"><i class="fa-brands fa-instagram"></i></a>
                 </div>
-                <span>or use your email password</span>
-                <input type="email" name="email" id="email" placeholder="Email">
-                <input type="password" name="password" id="password" placeholder="Password">
-                <a href="forgot-password.php">Forgot Your Password?</a>
-                <button type="submit" name="submit" id="submit">Sign In</button>
+                <span>or use your email for registeration</span>
+                <input type="text" placeholder="Name">
+                <input type="email" placeholder="Email" id="email">
+                <input type="password" placeholder="Password" id="password">
+                <button>Sign Up</button>
             </form>
         </div>
-        
+        <div class="form-container sign-in">
+            <form method="post">
+                <a href="https://etsbi.edu.ba/" class="icon" target="_blank">
+                    <img src="imgs/dwa.png" alt="ETSBI" class="custom-icon">
+                    
+                </a>
+                <h1>Prijavite se</h1>
+                <div class="social-icons">
+                    
+                    <a href="https://etsbi.edu.ba/" class="icon"><i class="fa-solid fa-graduation-cap"></i></a>
+                    <a href="https://www.facebook.com/etsbi.bihac/?locale=hr_HR" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="https://www.instagram.com/etsbi_/" class="icon"><i class="fa-brands fa-instagram"></i></a>
+                </div>
+
+                <span>upotrebite svoj email i lozinku za pristup.</span>
+                <input type="email" placeholder="Email" name="email" id="email">
+                <input type="password" placeholder="Lozinka" name="password" id="password">
+                <a href="forgot-password.php">Zaboravili ste lozinku?</a>
+                <button type="submit" name="submit" id="submit">Prijavite se</button>
+                
+            </form>
+        </div>
         <div class="toggle-container">
             <div class="toggle">
+                <div class="toggle-panel toggle-left">
+                    <h1>Zaštićen pristup!</h1>
+                    <p>Enter your personal details to use all of site features</p>
+                    <button class="hidden" id="login">Sign In</button>
+                </div>
                 <div class="toggle-panel toggle-right">
-                    <h1>Hello, Friend!</h1>
-                    <p>SignIn with your personal details to use all of site features</p>
+                
+                    <h1>Zaštićen pristup!</h1>
+
+                    <p>Samo ovlašteni korisnici mogu pristupiti ovom sistemu.</p>
+                    
                 </div>
             </div>
         </div>
     </div>
+
 </body>
 
 </html>
